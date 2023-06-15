1930e86ab8334065bcff8ffd84033f6c7195d1b1
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-06-15 18:01:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [count, setCount] = useState(0)
   return (
     <div>
         {/* Do not remove the main div */}
